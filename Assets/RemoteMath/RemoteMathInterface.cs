eb9a23b2db7d831c887091d482a9f95cf1d60272
MrMelon54 <github@mrmelon54.com>
2023-08-17 03:25:37 +0100

    Some module improvements

--- Assets/RemoteMath/RemoteMathInterface.cs
+++ Assets/RemoteMath/RemoteMathInterface.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Runtime.InteropServices;
+using System.Globalization;
+using System.IO;
 using UnityEngine;
 using KeepCoding;
 
@@ -7,56 +8,89 @@ namespace RemoteMath
 {
     public static class RemoteMathInterface
     {
-        [DllImport("ktanemod-remote-math-interface", CallingConvention = CallingConvention.Cdecl)]
-        private static extern void RemoteMathInterfaceEntry();
-
-		[DllImport("ktanemod-remote-math-interface", CallingConvention = CallingConvention.Cdecl)]
-		private static extern void RemoteMathIsEditor();
-		
-        [DllImport("ktanemod-remote-math-interface", CallingConvention = CallingConvention.Cdecl)]
-        private static extern void RemoteMathNotEditor();
-
         public static void Entry(bool useProdAddressInDev)
         {
-			if (Application.isEditor)
+            string asmPath = GetAssemblyPath("ktanemod-remote-math-interface");
+            if (Application.isEditor)
             {
-                RemoteMathIsEditor();
-                if (useProdAddressInDev) RemoteMathNotEditor();
-                RemoteMathInterfaceEntry();
-            } else Experimental.UnmanagedExternalMethod(GetAssemblyPath("ktanemod-remote-math-interface"), "RemoteMathInterfaceEntry").Invoke();
+                Experimental.UnmanagedExternalMethod(asmPath, "RemoteMathIsEditor").Invoke();
+                if (useProdAddressInDev)
+                    Experimental.UnmanagedExternalMethod(asmPath, "RemoteMathNotEditor").Invoke();
+            }
+
+            Experimental.UnmanagedExternalMethod(asmPath, "RemoteMathInterfaceEntry").Invoke();
         }
 
-		// Windows: <path>/dlls/<arch>/<file>.dll
-        // OSX: <path>/dlls/<file>.dylib
-        // Linux: <path>/dlls/lib<file>.so
-        private static String GetAssemblyPath(String file)
+        // Get CPU architecture
+        private static String GetArch()
         {
-            string path = PathManager.GetDirectory();
-            const string target = "dlls";
             string arch;
+            bool _64;
+
             switch (IntPtr.Size)
             {
                 case 4:
-                    arch = "x86";
+                    _64 = false;
                     break;
                 case 8:
-                    arch = "x86_64";
+                    _64 = true;
                     break;
                 default:
-                    throw new PlatformNotSupportedException("IntPtr's size is not 4 or 8. Only 32-bit and 64-bit devices are supported.");
+                    throw new PlatformNotSupportedException(
+                        "IntPtr's size is not 4 or 8. Only 32-bit and 64-bit devices are supported.");
             }
 
+            if (CultureInfo.InvariantCulture.CompareInfo.IndexOf(SystemInfo.processorType, "ARM",
+                    CompareOptions.IgnoreCase) >= 0)
+                arch = _64 ? "arm64" : "arm";
+            else
+                arch = _64 ? "amd64" : "386";
+
+            return arch;
+        }
+
+        // Windows: <path>/dlls/<file>-windows-<arch>.dll
+        // OSX: <path>/dlls/<file>-darwin-<arch>.dylib
+        // Linux: <path>/dlls/lib<file>-linux-<arch>.so
+        private static String GetAssemblyPath(String file)
+        {
+            bool editor = Application.isEditor;
+            string path = editor ? Path.Combine(Application.dataPath, "Plugins") : PathManager.GetDirectory();
+            const string target = "dlls";
+            string arch = GetArch();
+
+            string p;
             switch (Application.platform)
             {
-                case RuntimePlatform.WindowsPlayer:
-                    return string.Format(@"{0}\{1}\{2}\{3}.{4}", path, target, arch, file, "dll");
-                case RuntimePlatform.OSXPlayer:
-                    return string.Format("{0}/{1}/{2}.{3}", path, target, file, "dylib");
+                case RuntimePlatform.LinuxEditor:
                 case RuntimePlatform.LinuxPlayer:
-                    return string.Format("{0}/{1}/lib{2}.{3}", path, target, file, "so");
+                    p = string.Format("{0}/{1}/lib{2}-linux-{3}.{4}", path, target, file, arch, "so");
+                    break;
+                case RuntimePlatform.WindowsEditor:
+                case RuntimePlatform.WindowsPlayer:
+                    p = string.Format(@"{0}\{1}\{2}-windows-{3}.{4}", path, target, file, arch, "dll");
+                    break;
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.OSXPlayer:
+                    p = string.Format("{0}/{1}/{2}-darwin-{3}.{4}", path, target, file, arch, "dylib");
+                    break;
                 default:
-                    throw new PlatformNotSupportedException("The platform \"" + Application.platform + "\" is unsupported. The operating systems supported are Windows, Mac, and Linux.");
+                    throw new PlatformNotSupportedException("The platform \"" + Application.platform +
+                                                            "\" is unsupported. The operating systems supported are Windows, Mac, and Linux.");
             }
+
+            Debug.Log("Loading in editor: " + Application.isEditor);
+            Debug.Log("Path: " + path);
+            Debug.Log("Using DLL: " + p);
+
+            if (!File.Exists(p))
+            {
+                throw new PlatformNotSupportedException("The platform \"" + Application.platform +
+                                                        "\" does not support the architecture \"" + arch +
+                                                        "\", please notify the developer.");
+            }
+
+            return p;
         }
     }
 }
